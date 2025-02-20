2e07b796de880cc3de7e45c8085b2dc5abe16b4a
lemosjose <devlemosjose@gmail.com>
2025-02-20 09:30:10 -0300

    finaliza interface

---
+++ src/connection.py
@@ -0,0 +1,17 @@
+# This Python file uses the following encoding: utf-8
+
+import os
+
+class connection:
+    def __init__(self, self.user, self.passwd):
+        usuario =
+        senha =
+
+    def createConnection(self ):
+        os.command(ssh, self.usuario )
+        if():
+            ("não foi possivel conectar a sua vps informada, por favor tente outro usuario e/ou senha")
+            return False
+
+        return True
+
--- ui_form.py
+++ ui_form.py
@@ -16,123 +16,113 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialogButtonBox, QLabel,
-    QSizePolicy, QSpacerItem, QTextBrowser, QTextEdit,
-    QVBoxLayout, QWidget)
+    QPlainTextEdit, QSizePolicy, QTabWidget, QTextEdit,
+    QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(423, 817)
-        self.verticalLayoutWidget = QWidget(Widget)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 421, 811))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.label_3 = QLabel(self.verticalLayoutWidget)
-        self.label_3.setObjectName(u"label_3")
-
-        self.verticalLayout.addWidget(self.label_3)
-
-        self.textBrowser_5 = QTextBrowser(self.verticalLayoutWidget)
-        self.textBrowser_5.setObjectName(u"textBrowser_5")
-
-        self.verticalLayout.addWidget(self.textBrowser_5)
-
-        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer_5)
-
-        self.label_5 = QLabel(self.verticalLayoutWidget)
-        self.label_5.setObjectName(u"label_5")
-
-        self.verticalLayout.addWidget(self.label_5)
-
-        self.textBrowser_3 = QTextBrowser(self.verticalLayoutWidget)
-        self.textBrowser_3.setObjectName(u"textBrowser_3")
-
-        self.verticalLayout.addWidget(self.textBrowser_3)
-
-        self.horizontalSpacer_6 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer_6)
-
-        self.label_6 = QLabel(self.verticalLayoutWidget)
-        self.label_6.setObjectName(u"label_6")
-
-        self.verticalLayout.addWidget(self.label_6)
-
-        self.textEdit = QTextEdit(self.verticalLayoutWidget)
-        self.textEdit.setObjectName(u"textEdit")
-
-        self.verticalLayout.addWidget(self.textEdit)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer)
-
-        self.label_2 = QLabel(self.verticalLayoutWidget)
-        self.label_2.setObjectName(u"label_2")
-
-        self.verticalLayout.addWidget(self.label_2)
-
-        self.textBrowser_2 = QTextBrowser(self.verticalLayoutWidget)
-        self.textBrowser_2.setObjectName(u"textBrowser_2")
-
-        self.verticalLayout.addWidget(self.textBrowser_2)
-
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer_2)
-
-        self.label = QLabel(self.verticalLayoutWidget)
-        self.label.setObjectName(u"label")
-
-        self.verticalLayout.addWidget(self.label)
-
-        self.textBrowser = QTextBrowser(self.verticalLayoutWidget)
-        self.textBrowser.setObjectName(u"textBrowser")
-
-        self.verticalLayout.addWidget(self.textBrowser)
-
-        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer_3)
-
-        self.label_4 = QLabel(self.verticalLayoutWidget)
+        Widget.resize(454, 661)
+        self.textEdit_7 = QTextEdit(Widget)
+        self.textEdit_7.setObjectName(u"textEdit_7")
+        self.textEdit_7.setGeometry(QRect(10, 330, 439, 21))
+        self.label_4 = QLabel(Widget)
         self.label_4.setObjectName(u"label_4")
-
-        self.verticalLayout.addWidget(self.label_4)
-
-        self.textBrowser_4 = QTextBrowser(self.verticalLayoutWidget)
-        self.textBrowser_4.setObjectName(u"textBrowser_4")
-
-        self.verticalLayout.addWidget(self.textBrowser_4)
-
-        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.verticalLayout.addItem(self.horizontalSpacer_4)
-
-        self.buttonBox = QDialogButtonBox(self.verticalLayoutWidget)
+        self.label_4.setGeometry(QRect(10, 250, 127, 18))
+        self.textEdit_3 = QTextEdit(Widget)
+        self.textEdit_3.setObjectName(u"textEdit_3")
+        self.textEdit_3.setEnabled(True)
+        self.textEdit_3.setGeometry(QRect(10, 200, 439, 21))
+        self.label_7 = QLabel(Widget)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setGeometry(QRect(10, 310, 142, 18))
+        self.label_8 = QLabel(Widget)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setGeometry(QRect(10, 510, 150, 18))
+        self.textEdit_6 = QTextEdit(Widget)
+        self.textEdit_6.setObjectName(u"textEdit_6")
+        self.textEdit_6.setGeometry(QRect(10, 270, 81, 21))
+        self.textEdit = QTextEdit(Widget)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(11, 81, 439, 21))
+        self.label_5 = QLabel(Widget)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(10, 120, 100, 18))
+        self.textEdit_2 = QTextEdit(Widget)
+        self.textEdit_2.setObjectName(u"textEdit_2")
+        self.textEdit_2.setGeometry(QRect(10, 140, 439, 21))
+        self.buttonBox = QDialogButtonBox(Widget)
         self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setGeometry(QRect(280, 630, 166, 26))
         self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
-
-        self.verticalLayout.addWidget(self.buttonBox)
-
+        self.textEdit_4 = QTextEdit(Widget)
+        self.textEdit_4.setObjectName(u"textEdit_4")
+        self.textEdit_4.setGeometry(QRect(10, 470, 441, 21))
+        self.textEdit_4.setMaximumSize(QSize(14777215, 14777215))
+        self.plainTextEdit = QPlainTextEdit(Widget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit.setGeometry(QRect(10, 530, 441, 91))
+        self.label_6 = QLabel(Widget)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(10, 180, 191, 18))
+        self.tabWidget = QTabWidget(Widget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(11, 21, 431, 31))
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+        self.tab_3 = QWidget()
+        self.tab_3.setObjectName(u"tab_3")
+        self.tabWidget.addTab(self.tab_3, "")
+        self.tab_4 = QWidget()
+        self.tab_4.setObjectName(u"tab_4")
+        self.tabWidget.addTab(self.tab_4, "")
+        self.tab_5 = QWidget()
+        self.tab_5.setObjectName(u"tab_5")
+        self.tabWidget.addTab(self.tab_5, "")
+        self.tab_6 = QWidget()
+        self.tab_6.setObjectName(u"tab_6")
+        self.tabWidget.addTab(self.tab_6, "")
+        self.label_2 = QLabel(Widget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 450, 186, 18))
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 370, 126, 18))
+        self.label_3 = QLabel(Widget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(11, 57, 85, 18))
+        self.textEdit_5 = QTextEdit(Widget)
+        self.textEdit_5.setObjectName(u"textEdit_5")
+        self.textEdit_5.setGeometry(QRect(10, 390, 441, 41))
 
         self.retranslateUi(Widget)
+
+        self.tabWidget.setCurrentIndex(3)
+
 
         QMetaObject.connectSlotsByName(Widget)
     # setupUi
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
-        self.label_3.setText(QCoreApplication.translate("Widget", u"IP da sua vps", None))
+        self.label_4.setText(QCoreApplication.translate("Widget", u"Porta SSH utilizada", None))
+        self.label_7.setText(QCoreApplication.translate("Widget", u"Dominio/Subdominio", None))
+        self.label_8.setText(QCoreApplication.translate("Widget", u"Output dos Comandos", None))
         self.label_5.setText(QCoreApplication.translate("Widget", u"Usuario da VPS", None))
-        self.label_6.setText(QCoreApplication.translate("Widget", u"Senha do Uusario na VPS", None))
+        self.label_6.setText(QCoreApplication.translate("Widget", u"Senha do Usu\u00e1rio na sua VPS", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Widget", u"Tab 1", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Widget", u"Tab 2", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("Widget", u"Page", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QCoreApplication.translate("Widget", u"Page", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_5), QCoreApplication.translate("Widget", u"Page", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_6), QCoreApplication.translate("Widget", u"Page", None))
         self.label_2.setText(QCoreApplication.translate("Widget", u"senha do novo usuario smtp", None))
         self.label.setText(QCoreApplication.translate("Widget", u"Novo usuario smtp", None))
-        self.label_4.setText(QCoreApplication.translate("Widget", u"Porta SSH utilizada", None))
+        self.label_3.setText(QCoreApplication.translate("Widget", u"IP da sua vps", None))
     # retranslateUi
 
